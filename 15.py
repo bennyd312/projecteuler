da82100c107a78a4ef5874286f9a77a17f6b19ae
bennyd312 <112394158+bennyd312@users.noreply.github.com>
2023-07-14 18:21:46 +0200

    Add files via upload

---
+++ 15.py
@@ -0,0 +1,3 @@
+#https://projecteuler.net/problem=15
+#using combinatorics we figure out that the problem is equivalent to picking 20 elements from a group of 40 elements.
+#The answer therefore is the combination number C(40,20)
